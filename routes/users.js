ca4b39467b846479d66c9c213a9fab273972c861
Akshay-jain22 <akshayjain.191it102@nitk.edu.in>
2021-03-23 15:16:52 +0530

    Updated UI

--- routes/users.js
+++ routes/users.js
@@ -5,38 +5,38 @@ require('../services/auth/oauth');
 
 
 router.get("/auth/google/success", (req, res, next) => {
-  req.flash("success", "Successfully authenticated using Google Accounts");
-  res.redirect("/");
+    req.flash("success", "Successfully authenticated using Google Account");
+    res.redirect("/");
 });
 
 router.get("/auth/google/failure", (req, res, next) => {
-  req.flash("danger", "somthing went wrong, authentication failed!");
-  res.redirect("/");
+    req.flash("danger", "Something went wrong, Authentication failed!");
+    res.redirect("/");
 });
 
 router.get(
-  "/auth/google/callback",
-  passport.authenticate("google", {
-    successRedirect: "/auth/google/success",
-    failureRedirect: "/auth/google/failure",
-  })
+    "/auth/google/callback",
+    passport.authenticate("google", {
+        successRedirect: "/auth/google/success",
+        failureRedirect: "/auth/google/failure",
+    })
 );
 
 router.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["email", "profile"],
-  })
+    "/auth/google",
+    passport.authenticate("google", {
+        scope: ["email", "profile"],
+    })
 );
 
 router.get("/logout", (req, res, next) => {
-  if (req.session && req.user) {
-    req.logout();
-    req.flash("danger", "You are Successfully logged out!");
-  } else {
-    req.flash("danger", "You are already logged out!");
-  }
-  res.redirect("/");
+    if (req.session && req.user) {
+        req.logout();
+        req.flash("danger", "You are Successfully logged out!");
+    } else {
+        req.flash("danger", "You are already logged out!");
+    }
+    res.redirect("/");
 });
 
 module.exports = router;
