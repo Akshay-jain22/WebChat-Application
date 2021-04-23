de0f57195728cdc1e41d0a1590a54e19496b6c6e
Akshay Jain <akshayjain222002@gmail.com>
2021-04-24 00:27:14 +0530

    Improved chat routes

--- routes/chat.js
+++ routes/chat.js
@@ -5,12 +5,12 @@ const Connection = require('../db/model/connection');
 const MSG = require("../db/model/msg");
 const auth = require('../services/auth/authenticate');
 
-router.get('/msg', auth.authenticateUser, async (req, res, next) => {
+router.get('/chat', auth.authenticateUser, async(req, res, next) => {
     try {
         const connections = await Connection.find({})
             .sort({ updatedAt: 1 })
             .populate("user_id");
-        
+
         res.render("msg/index", {
             layout: "./layouts/msg",
             connections: connections,
@@ -20,26 +20,25 @@ router.get('/msg', auth.authenticateUser, async (req, res, next) => {
     }
 });
 
-router.get('/msg/chat/:uid', auth.authenticateUser, async (req, res, next) => {
-  try {
-    const U1_ID = req.user._id;
-    const U2_ID = req.params.uid;
+router.get('/chat/chat/:uid', auth.authenticateUser, async(req, res, next) => {
+    try {
+        const U1_ID = req.user._id;
+        const U2_ID = req.params.uid;
 
-    const msgs = await MSG.find({
-      $or: [
-        {
-          $and: [{ sender_id: U1_ID }, { receiver_id: U2_ID }],
-        },
-        {
-          $and: [{ sender_id: U2_ID }, { receiver_id: U1_ID }],
-        },
-      ],
-    }).sort({ updatedAt: 1 });
+        const msgs = await MSG.find({
+            $or: [{
+                    $and: [{ sender_id: U1_ID }, { receiver_id: U2_ID }],
+                },
+                {
+                    $and: [{ sender_id: U2_ID }, { receiver_id: U1_ID }],
+                },
+            ],
+        }).sort({ updatedAt: 1 });
 
-    res.status(200);
-    res.json(msgs);
-  } catch (err) {
-    next(err);
-  }
+        res.status(200);
+        res.json(msgs);
+    } catch (err) {
+        next(err);
+    }
 });
 module.exports = router;
