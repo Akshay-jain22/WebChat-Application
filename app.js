b9ea83b25924c541b21d495c2af0df7f6eb36793
Akshay Jain <akshayjain222002@gmail.com>
2021-04-24 00:11:47 +0530

    Enhanced Express Server for readability

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // node modules 
-var createError  = require('http-errors');
-var express      = require('express');
-var path         = require('path');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger       = require('morgan');
-const session    = require("express-session");
+var logger = require('morgan');
+const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
-const passport   = require("passport");
-const flash      = require("connect-flash");
-const layout     = require('express-ejs-layouts');
+const passport = require("passport");
+const flash = require("connect-flash");
+const layout = require('express-ejs-layouts');
 require("dotenv").config();
 
 // local node modules
@@ -39,11 +39,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 // passport configuration
 // ----------------------------------------
 app.use(
-  session({
-    key: 'chat-app',
-    secret: 'chat-app',
-    store: new MongoStore({ url: process.env.DATABASE_URI })
-  }),
+    session({
+        key: 'chat-app',
+        secret: 'chat-app',
+        store: new MongoStore({ url: process.env.DATABASE_URI })
+    }),
 );
 app.use(passport.initialize());
 app.use(passport.session());
@@ -54,10 +54,10 @@ app.use(flash());
 // set local variables 
 // ----------------------------------------
 app.use((req, res, next) => {
-  res.locals.current_user = req.user;
-  res.locals.danger       = req.flash('danger');
-  res.locals.success      = req.flash('success');
-  next();
+    res.locals.current_user = req.user;
+    res.locals.danger = req.flash('danger');
+    res.locals.success = req.flash('success');
+    next();
 });
 
 
@@ -68,18 +68,18 @@ app.use(gameRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 // app.get('/', (req,res)=>{
